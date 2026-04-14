560991a1033ee4f14566137cb24149988a9efc6c
devgsj <1577432674@qq.com>
2026-04-14 14:24:01 +0800

    Simplify UIComponent rendering with solid color and centered text

--- src/engine/uiComponent.c
+++ src/engine/uiComponent.c
@@ -32,18 +32,12 @@ void updateUIComponent(Component *comp)
     // 更新逻辑 (这里可以添加事件处理，如鼠标点击等)
     UIComponent *uiComp = (UIComponent *)comp;
 
-    // 简单的点击检测：只在鼠标按下时触发一次
-    static int mousePressed = 0;
+    // 检查是否有点击事件触发
     int mouseX, mouseY;
     Uint32 buttons = SDL_GetMouseState(&mouseX, &mouseY);
-
-    int isHovering = mouseX > uiComp->rect.x && mouseX < (uiComp->rect.x + uiComp->rect.w) &&
-                     mouseY > uiComp->rect.y && mouseY < (uiComp->rect.y + uiComp->rect.h);
-
-    int isPressed = (buttons & SDL_BUTTON(SDL_BUTTON_LEFT)) != 0;
-
-    // 检测鼠标按下瞬间
-    if (isPressed && !mousePressed && isHovering)
+    if ((buttons & SDL_BUTTON(SDL_BUTTON_LEFT)) &&
+        mouseX > uiComp->rect.x && mouseX < (uiComp->rect.x + uiComp->rect.w) &&
+        mouseY > uiComp->rect.y && mouseY < (uiComp->rect.y + uiComp->rect.h))
     {
         // 调用点击回调函数
         if (uiComp->onClick)
@@ -51,8 +45,6 @@ void updateUIComponent(Component *comp)
             uiComp->onClick(comp);
         }
     }
-
-    mousePressed = isPressed;
 }
 
 void renderUIComponent(Component *component, SDL_Renderer *renderer)
@@ -60,38 +52,38 @@ void renderUIComponent(Component *component, SDL_Renderer *renderer)
     UIComponent *uiComp = (UIComponent *)component;
 
     // 设置弹窗背景阴影
-    SDL_Rect shadowRect = {uiComp->rect.x + 10, uiComp->rect.y + 10, uiComp->rect.w, uiComp->rect.h};
+    SDL_Rect shadowRect = {uiComp->rect.x + 5, uiComp->rect.y + 5, uiComp->rect.w, uiComp->rect.h};
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 128); // 半透明的黑色阴影
     SDL_RenderFillRect(renderer, &shadowRect);
 
-    // 渐变背景
-    SDL_Rect popupRect = uiComp->rect;
-    SDL_Color startColor = {200, 200, 200, 255}; // 渐变的起始颜色
-    SDL_Color endColor = {255, 255, 255, 255};   // 渐变的结束颜色
-
-    // 简单的垂直渐变
-    for (int i = 0; i < popupRect.h; ++i)
-    {
-        Uint8 r = startColor.r + (endColor.r - startColor.r) * i / popupRect.h;
-        Uint8 g = startColor.g + (endColor.g - startColor.g) * i / popupRect.h;
-        Uint8 b = startColor.b + (endColor.b - startColor.b) * i / popupRect.h;
-
-        SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-        SDL_RenderDrawLine(renderer, popupRect.x, popupRect.y + i, popupRect.x + popupRect.w, popupRect.y + i);
-    }
+    // 使用纯色背景
+    SDL_SetRenderDrawColor(renderer, uiComp->color.r, uiComp->color.g, uiComp->color.b, uiComp->color.a);
+    SDL_RenderFillRect(renderer, &uiComp->rect);
 
     // 添加弹窗边框
     SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255); // 深灰色边框
-    SDL_RenderDrawRect(renderer, &popupRect);
+    SDL_RenderDrawRect(renderer, &uiComp->rect);
 
     // 渲染文字
     if (uiComp->text && uiComp->font)
     {
         SDL_Surface *surface = TTF_RenderText_Solid(uiComp->font, uiComp->text, uiComp->textColor);
-        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_Rect textRect = {uiComp->rect.x, uiComp->rect.y, surface->w, surface->h};
-        SDL_RenderCopy(renderer, texture, NULL, &textRect);
-        SDL_DestroyTexture(texture);
-        SDL_FreeSurface(surface);
+        if (surface)
+        {
+            SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+            if (texture)
+            {
+                // 居中文字
+                SDL_Rect textRect = {
+                    uiComp->rect.x + (uiComp->rect.w - surface->w) / 2,
+                    uiComp->rect.y + (uiComp->rect.h - surface->h) / 2,
+                    surface->w,
+                    surface->h
+                };
+                SDL_RenderCopy(renderer, texture, NULL, &textRect);
+                SDL_DestroyTexture(texture);
+            }
+            SDL_FreeSurface(surface);
+        }
     }
 }
